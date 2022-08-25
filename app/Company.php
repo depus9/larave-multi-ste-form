2d7bd59e1dda4eb91193b968cf9e33361cd2326e
depus9 <depus9@gmail.com>
2022-08-25 09:45:14 +0545

    register app fields

---
+++ app/Company.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App;
+
+
+
+use Illuminate\Database\Eloquent\Model;
+
+
+
+class Company extends Model
+
+{
+
+    /**
+
+     * The attributes that are mass assignable.
+
+     *
+
+     * @var array
+
+     */
+
+    protected $fillable = [
+        'company_name', 'company_address', 'company_email', 'company_phone', 'contact_name', 'contact_email', 'contact_phone', 'company_status',
+        'company_is_premium', 'company_register_date', 'company_expiry_date', 'company_renewal_date'
+
+    ];
+}
