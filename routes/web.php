5c43b3f991214366dacbff7096ba26394b3aae0e
depus9 <depus9@gmail.com>
2022-08-25 09:50:14 +0545

    router added to the app

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('company_profile', 'CompanyController@index')->name('company_profile.index');
+Route::get('company_profile/create-step-one', 'CompanyController@createStepOne')->name('company_profile.create.step.one');
+Route::post('company_profile/create-step-one', 'CompanyController@postCreateStepOne')->name('company_profile.create.step.one.post');
+Route::get('company_profile/create-step-two', 'CompanyController@createStepTwo')->name('company_profile.create.step.two');
+Route::post('company_profile/create-step-two', 'CompanyController@postCreateStepTwo')->name('company_profile.create.step.two.post');
+Route::get('company_profile/create-step-three', 'CompanyController@createStepThree')->name('company_profile.create.step.three');
+Route::post('company_profile/create-step-three', 'CompanyController@postCreateStepThree')->name('company_profile.create.step.three.post');
